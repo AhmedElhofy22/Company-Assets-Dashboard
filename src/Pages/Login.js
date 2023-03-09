ee2bccf6a53d269a68f479438a252ce5cb6c9fdc
messi204 <ahmedelhofy8686@gmail.com>
2023-03-09 16:40:15 +0200

    finish login and signup pages

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import Logo from '../Images/shopify-ar21.svg'
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const loginEmailHandler=(e)=>{
+    setEmail(e.target.value)
+  }
+  const loginPasswordHandler=(e)=>{
+    setPassword(e.target.value)
+  }
+  const navigate=useNavigate()
+  const goToSignUpPage=()=>{
+    navigate('/signup')
+  }
   return (
     <div className="login">
       <img src={Logo} alt='logo-img' className="login-logo"/>
@@ -10,15 +23,15 @@ const Login = () => {
       <h1>Login</h1>
       <form >
         <h5>Email</h5>
-        <input placeholder="Email" type="email" value="" />
+        <input placeholder="Email" type="email" value={email} onChange={loginEmailHandler}/>
         <h5>Password</h5>
-        <input placeholder="Password" type="password" value="" />
+        <input placeholder="Password" type="password" value={password} onChange={loginPasswordHandler}/>
         <button type="submit" className="login-signInBtn">Sign In</button>
         <p>
         By continuing, you agree to our Conditions of Use
         and Privacy Notice.
         </p>
-       <button className="login-registerBtn" >
+       <button type="button"  className="login-registerBtn" onClick={goToSignUpPage} >
         Create Your Shopify Account
        </button>
       </form>
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../Images/shopify-ar21.svg'
 import './SignUp.css'
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [signUpName, setSignUpName] = useState("")
+  const [signUpEmail, setSignUpEmail] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
+  const [resetPassword, setResetPassword] = useState("")
+  const signUpNameHandler=(e)=>{
+    setSignUpName(e.target.value)
+  }
+  const signUpEmailHandler=(e)=>{
+    setSignUpEmail(e.target.value)
+  }
+  const signUpPasswordHandler=(e)=>{
+    setSignUpPassword(e.target.value)
+  }
+  const resetPasswordHandler=(e)=>{
+    setResetPassword(e.target.value)
+  }
   return (
     <div className="signUp">
     <img src={Logo} alt='logo-img' className="signUp-logo"/>
@@ -10,18 +27,19 @@ const SignUp = () => {
     <h1>Create Account</h1>
     <form >
       <h5>Your Name</h5>
-      <input placeholder="First & Last Name" type="text" value="" />
+      <input placeholder="First & Last Name" type="text" value={signUpName} onChange={signUpNameHandler}/>
       <h5>Email</h5>
-      <input placeholder="Email" type="email" value="" />
+      <input placeholder="Email" type="email" value={signUpEmail} onChange={signUpEmailHandler}/>
       <h5>Password</h5>
-      <input placeholder="Password" type="password" value="" />
+      <input placeholder="Password" type="password" value={signUpPassword} onChange={signUpPasswordHandler}/>
       <h5>Re-Enter Password</h5>
-      <input  type="password" value="" />
+      <input  type="password" value={resetPassword} onChange={resetPasswordHandler}/>
       <button type="submit" className="signUp-signUpBtn">Sign Up</button>
       <p>
       By creating an account, you agree to our Conditions of Use
       and Privacy Notice.
       </p>
+      <p>Already Have An Account?<Link to="/login">Sign In</Link></p>
     
     </form>
   </div>
